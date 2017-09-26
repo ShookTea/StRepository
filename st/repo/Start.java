b689cb4b4e78575405d55a627aae777e31e470ed
Norbert Kowalik <kowaliknor@gmail.com>
2017-09-26 13:23:29 +0200

    Commit startowy

---
+++ st/repo/Start.java
@@ -0,0 +1,15 @@
+package st.repo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
