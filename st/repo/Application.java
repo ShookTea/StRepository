9dc59b8bd5d83cee2e72102a87d59ea6a8ed87e1
Norbert Kowalik <kowaliknor@gmail.com>
2017-09-26 15:18:40 +0200

    Rozszerzenie klasy Application o nazwę programu

--- st/repo/Application.java
+++ st/repo/Application.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    public final StringProperty title;
     public final BooleanProperty isDownloaded;
     public final BooleanProperty canBeUpdated;
     public final BooleanProperty isLocked;
@@ -20,7 +21,8 @@ public class Application {
     public final ObservableList<Link> authors;
     public final ObservableList<Link> links;
 
-    private Application(boolean isDownloaded, boolean canBeUpdated, boolean isLocked, String installedVersion, String newestVersion, String description, List<Link> authors, List<Link> links) {
+    private Application(String title, boolean isDownloaded, boolean canBeUpdated, boolean isLocked, String installedVersion, String newestVersion, String description, List<Link> authors, List<Link> links) {
+        this.title = new SimpleStringProperty(title);
         this.isDownloaded = new SimpleBooleanProperty(isDownloaded);
         this.canBeUpdated = new SimpleBooleanProperty(canBeUpdated);
         this.isLocked = new SimpleBooleanProperty(isLocked);
@@ -31,5 +33,5 @@ public class Application {
         this.links = FXCollections.observableArrayList(links);
     }
 
-    public static final Application NULL_APP = new Application(false, false, true, "", "", "", new ArrayList<>(), new ArrayList<>());
+    public static final Application NULL_APP = new Application("", false, false, true, "", "", "", new ArrayList<>(), new ArrayList<>());
 }
