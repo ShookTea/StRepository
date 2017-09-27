03561629e99f8bbfc8a397190299f00ad49c83ce
Norbert Kowalik <kowaliknor@gmail.com>
2017-09-27 13:57:37 +0200

    Odczytywanie rozmiaru pliku i jego nazwy

--- st/repo/InstallationData.java
+++ st/repo/InstallationData.java
@@ -6,7 +6,12 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.StringProperty;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class InstallationData {
     public final URL downloadUrl;
@@ -25,6 +30,24 @@ public class InstallationData {
             System.err.println("Error: cannot create folder " + folder.getPath());
             System.exit(0);
         }
+        try {
+            download(downloadUrl, folder);
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (!folder.delete()) folder.deleteOnExit();
+            System.exit(0);
+        }
+    }
+
+    private void download(URL url, File folder) throws IOException {
+        URLConnection urlc = url.openConnection();
+        String urlFile = url.getFile();
+        String fileName = urlFile.substring(urlFile.lastIndexOf('/')+1, urlFile.length());
+        System.out.println("FILE NAME: " + fileName);
+        double bytes = urlc.getContentLength();
+        //InputStream stream = urlc.getInputStream();
+        //ReadableByteChannel rbc = Channels.newChannel(stream);
+
     }
 
     public static final DoubleProperty installationPhase = new SimpleDoubleProperty(0.0);
