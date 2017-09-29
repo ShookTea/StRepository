e937303a6bbff07bae00b30860d403733347cc4a
Norbert Kowalik <kowaliknor@gmail.com>
2017-09-29 16:40:29 +0200

    Umożliwienie obsługi usuwania rozszerzenia z systemu

--- st/repo/reg/Registry.java
+++ st/repo/reg/Registry.java
@@ -9,6 +9,14 @@ public abstract class Registry {
     protected abstract boolean createExtensionForUser(Extension ext);
     protected abstract boolean createExtensionForAdmin(Extension ext);
 
+    public boolean removeExtension(Extension ext) {
+        if (removeExtensionForAdmin(ext)) return true;
+        return removeExtensionForUser(ext);
+    }
+
+    protected abstract boolean removeExtensionForUser(Extension ext);
+    protected abstract boolean removeExtensionForAdmin(Extension ext);
+
     public static Registry getInstance() {
         return null;
     }
