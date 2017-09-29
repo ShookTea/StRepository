a6c5491923dbdae48fea25459f2b0b8f57f3fc9c
Norbert Kowalik <kowaliknor@gmail.com>
2017-09-29 15:48:32 +0200

    Klasa Registry będzie próbowała najpierw dodać rozszerzenie dla całego systemu (jeżeli program jest uruchomiony z uprawnieniami administratora). Jeżeli to się nie uda, spróbuje zainstalować rozszerzenie tylko dla danego użytkownika

--- st/repo/reg/Registry.java
+++ st/repo/reg/Registry.java
@@ -1,6 +1,13 @@
 package st.repo.reg;
 
 public abstract class Registry {
+    public boolean createExtension(Extension ext) {
+        if (createExtensionForAdmin(ext)) return true;
+        return createExtensionForUser(ext);
+    }
+
+    protected abstract boolean createExtensionForUser(Extension ext);
+    protected abstract boolean createExtensionForAdmin(Extension ext);
 
     public static Registry getInstance() {
         return null;
