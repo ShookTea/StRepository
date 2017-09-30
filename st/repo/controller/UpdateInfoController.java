1d62c8454e2c006646a382eb176c6a01481561e2
Norbert Kowalik <kowaliknor@gmail.com>
2017-09-30 13:38:45 +0200

    Okno informacji o aktualizacji

---
+++ st/repo/controller/UpdateInfoController.java
@@ -0,0 +1,33 @@
+package st.repo.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class UpdateInfoController {
+
+    @FXML
+    private Label appName;
+
+    @FXML
+    private Button yesButton;
+
+    @FXML
+    private Button noButton;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    void installUpdate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void runApplication(ActionEvent event) {
+
+    }
+
+}
